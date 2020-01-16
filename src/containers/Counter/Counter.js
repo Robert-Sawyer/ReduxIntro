7a4db68886b0b55b55a98babbb377dcc0bf87801
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-01-16 20:14:12 +0100

    Connected the Store to React.

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -6,29 +7,29 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 class Counter extends Component {
     state = {
         counter: 0
-    }
+    };
 
     counterChangedHandler = ( action, value ) => {
         switch ( action ) {
             case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } );
                 break;
             case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } );
                 break;
             case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } );
                 break;
             case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } );
                 break;
         }
-    }
+    };
 
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
+                <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
@@ -38,4 +39,11 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+//za pomocą tej funkcji ustawiamy state jako properties i zmieniamy value w CounterOutput ze state.counter na props.ctr (jak counter)
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter
+    }
+};
+
+export default connect(mapStateToProps)(Counter);

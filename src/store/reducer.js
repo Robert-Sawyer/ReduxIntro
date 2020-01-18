c2858ff261c0f8405b319ff8f5f0ad1ac887b1d6
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-01-18 12:32:44 +0100

    Modified reducer - replace 'if' by switch.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,27 +3,26 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-            //nie kopiujemy state za pomocą ... bo utworzyliśmy już initialstate, które jest kopią i ustawiamy jej nową wartość
-            counter: state.counter + 1
-        }
-    }
-    if (action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    }
-    if (action.type === 'ADD') {
-        return {
-        //action to po prostu zawartość nawiasów klamrowych w Counter, mapDispatchToProps, dispatch, możemy dodawać type propsów ile chcemy
-            counter: state.counter + action.value
-        }
-    }
-    if (action.type === 'SUBSTRACT') {
-        return {
-            counter: state.counter - action.value
-        }
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                //nie kopiujemy state za pomocą ... bo utworzyliśmy już initialstate, które jest kopią i ustawiamy jej nową wartość
+                counter: state.counter + 1
+            };
+            //nie musimy dawać break bo gdy wykona się jedna akcja nie ma możliwości że uruchomi się druga
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            };
+        case 'ADD':
+            return {
+                //action to po prostu zawartość nawiasów klamrowych w Counter, mapDispatchToProps, dispatch, możemy dodawać type propsów ile chcemy
+                counter: state.counter + action.value
+            };
+        case 'SUBSTRACT':
+            return {
+                counter: state.counter - action.value
+            }
     }
     return state;
 };

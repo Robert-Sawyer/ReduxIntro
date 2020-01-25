f92236b5157d0a3ff6826ca89e290c19c429c02e
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-01-25 15:23:41 +0100

    Outsourced action types to reducer and Counter.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions';
+
 const initialState = {
     counter: 0,
     results: []
@@ -5,7 +7,7 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'INCREMENT':
+        case actionTypes.INCREMENT:
             //nie chcemy zmieniać stanu state, czyli mutować go, tylko zachować go jako immutable, skopiować i wstawić nowa wartość
             //stary state wstawiamy w metodzie assign i klonujemy go w sposób immutable - tworzy NOWY obiekt ze wszystkimi
             //parametrami starego
@@ -13,7 +15,7 @@ const reducer = (state = initialState, action) => {
             newState.counter = state.counter + 1;
             return newState;
             //nie musimy dawać break bo gdy wykona się jedna akcja nie ma możliwości że uruchomi się druga
-        case 'DECREMENT':
+        case actionTypes.DECREMENT:
             return {
                 //ALTERNATYWNY SPOSÓB
                 //oto co sie tu dzieje: ... - weź obiekt state, skopiuj wszystkie jego wlasciwosci i wartosci, opakuj go
@@ -21,25 +23,25 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 counter: state.counter - 1
             };
-        case 'ADD':
+        case actionTypes.ADD:
             return {
                 ...state,
                 //action to po prostu zawartość nawiasów klamrowych w Counter, mapDispatchToProps, dispatch, możemy dodawać type propsów ile chcemy
                 counter: state.counter + action.value
             };
-        case 'SUBSTRACT':
+        case actionTypes.SUBTRACT:
             return {
                 ...state,
                 counter: state.counter - action.value
             };
-        case 'STORE_RESULT':
+        case actionTypes.STORE_RESULT:
             return {
                 ...state,
                 //concat działa jak push, tylko nie wrzuca wartości do starej tablicy, tylko tworzy NOWĄ, która równa sie
                 //starej PLUS argument w nawiasach, dlatego nie powinno sie uzywac push przy metodach ktore powinny byc immutable
                 results: state.results.concat({id: new Date, value: state.counter})
             };
-        case 'DELETE_RESULT':
+        case actionTypes.DELETE_RESULT:
             //są dwa sposoby na usunięcie elementu tablicy immutably - 1 poniżej: robimy kopię results, usuwamy obiekt z nowej
             //i podmieniamy tablice
             // const id = 2;

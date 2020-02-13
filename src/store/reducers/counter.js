ddb99654cc4aaa1b62b6de960f42cf20d603b91e
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2020-02-13 20:34:13 +0100

    Added utility functions in reducers to decrease some code.

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 //NIE POWINNO SIĘ DAWAĆ ZBYT DUŻO LOGIKI ZMIENIAJĄCEJ DANE DO ACTIONCREATORS, BO NIE OD TEGO JEST TA KLASA.
 // CAŁA LOGIKA MANIPULUJĄCA STANEM POWINNA SIĘ ZNALEŹĆ W REDUCERZE, BO TO REDUCER JEST JĄDREM PROJEKTU OPARTEGO NA REACT-REDUX
@@ -10,33 +11,15 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.INCREMENT:
-            //nie chcemy zmieniać stanu state, czyli mutować go, tylko zachować go jako immutable, skopiować i wstawić nowa wartość
-            //stary state wstawiamy w metodzie assign i klonujemy go w sposób immutable - tworzy NOWY obiekt ze wszystkimi
-            //parametrami starego
-            const newState = Object.assign({}, state);
-            newState.counter = state.counter + 1;
-            return newState;
-        //nie musimy dawać break bo gdy wykona się jedna akcja nie ma możliwości że uruchomi się druga
+            return updateObject(state, {counter: state.counter + 1});
         case actionTypes.DECREMENT:
-            return {
-                //ALTERNATYWNY SPOSÓB
-                //oto co sie tu dzieje: ... - weź obiekt state, skopiuj wszystkie jego wlasciwosci i wartosci, opakuj go
-                //w nowy obiekt ( {} ) i zmień tylko wartość pola counter, nie ruszaj result
-                ...state,
-                counter: state.counter - 1
-            };
+            return updateObject(state, {counter: state.counter - 1});
         case actionTypes.ADD:
-            return {
-                ...state,
-                //action to po prostu zawartość nawiasu klamrowego w metodzie add w actions.js,
-                //możemy dodawać type propsów ile chcemy
-                counter: state.counter + action.value
-            };
+            //action to po prostu zawartość nawiasu klamrowego w metodzie add w actions.js,
+            //możemy dodawać type propsów ile chcemy
+            return updateObject(state, {counter: state.counter + action.value});
         case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.value
-            };
+            return updateObject(state, {counter: state.counter - action.value});
     }
     return state;
 };
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 const initialState = {
     results: []
@@ -7,13 +8,10 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                //concat działa jak push, tylko nie wrzuca wartości do starej tablicy, tylko tworzy NOWĄ, która równa sie
-                //starej PLUS argument w nawiasach, dlatego nie powinno sie uzywac push przy metodach ktore powinny byc immutable
-                //UWAGA action.result
-                results: state.results.concat({id: new Date, value: action.result})
-            };
+            //concat działa jak push, tylko nie wrzuca wartości do starej tablicy, tylko tworzy NOWĄ, która równa sie
+            //starej PLUS argument w nawiasach, dlatego nie powinno sie uzywac push przy metodach ktore powinny byc immutable
+            //UWAGA action.result
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
         case actionTypes.DELETE_RESULT:
             //są dwa sposoby na usunięcie elementu tablicy immutably - 1 poniżej: robimy kopię results, usuwamy obiekt z nowej
             //i podmieniamy tablice
@@ -23,10 +21,7 @@ const reducer = (state = initialState, action) => {
 
             //filter zwraca nowa tablicę, nie dotyka orginalnej; przyjmuje funkcję jako argument i zwraca true albo false
             const updatedArray = state.results.filter(result => result.id !== action.resultElementId);
-            return {
-                ...state,
-                results: updatedArray
-            };
+            return updateObject(state, {results: updatedArray});
     }
     return state;
 };
